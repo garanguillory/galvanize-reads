19e0e4d620a965f84133c5bf2875e7361bdbf0a6
Garan Guillory <garanguillory@gmail.com>
2016-03-29 20:12:21 -0600

    Fix routes. Add/Edit new book working.

--- src/server/routes/books.js
+++ src/server/routes/books.js
@@ -6,18 +6,46 @@ function Books() {
   return knex('books');
 }
 
+
+// knex.select('first_name', 'last_name','biography','portrait_url','title','genre','description','cover_url')
+//     .from('authors')
+//     .rightJoin('booklists','authors.id','booklists.authors_id')
+//     .rightJoin('books','books.id','booklists.books_id');
+
+// knex.select('first_name', 'last_name','biography','portrait_url','title','genre','description','cover_url')
+//     .from('books')
+//     .rightJoin('booklists','books.id','booklists.books_id')
+//     .rightJoin('authors','authors.id','booklists.authors_id');
+
+// SELECT first_name, last_name, title, genre  
+// FROM authors
+// RIGHT JOIN booklists
+// ON authors.id=booklists.authors_id
+// RIGHT JOIN books
+// ON books.id=booklists.books_id;
+
+
 // WORKS
 // get ALL books
 router.get('/', function(req, res, next) {
-  Books().select('*').then(function(book){
-  	res.render('books',{programming_books: book });
-  })
+  // Books().select('*').then(function(book){
+  // 	res.render('books',{programming_books: book });
+  // })
+  knex.select('first_name', 'last_name','books_id','authors_id','title','genre','description','cover_url')
+      .from('authors')
+      .rightJoin('booklists','authors.id','booklists.authors_id')
+      .rightJoin('books','books.id','booklists.books_id')
+      .then(function(all){
+      	res.render('books', {authors_books:all});
+      });
 });
 
 // WORKS
 // get new book page
 router.get('/new', function(req, res, next) {
-  res.render('newbook');
+	knex.select('*').from('authors').then(function(author){
+		res.render('newbook',{author_list: author});
+	})
 });
 
 // WORKS
@@ -28,13 +56,34 @@ router.post('/new', function(req, res, next){
 	var genre = req.body.genre;
 	var description = req.body.description;
 	var cover_url = req.body.cover_url;
+	var authorsId = req.body.authorsId;
+	console.log(req.body);
 
 		Books().insert({
 			title: title,
 			genre: genre,
 			description: description,
 			cover_url: cover_url
-		}).then(function(data){
+		})
+		.returning('id')
+		.then(function (bookId) {
+			console.log('bookId', bookId);
+			knex('booklists').insert({
+				authors_id: authorsId,
+				books_id: bookId[0]
+			})
+			.returning('authors_id')
+			.then(function(authorsId){
+				console.log('authors_id', authorsId)
+			})
+			// create new rows in booklists where book_id is "bookId"
+			// and where the author id matches with the selected authors
+			// in the form.
+
+			// in order to resolve multiple promises,
+			// take a look at Promise.all
+		})
+		.then(function(data){
 			res.redirect('/books')
 		})
 });
@@ -84,13 +133,18 @@ router.post('/:id/edit', function(req, res, next){
 });
 
 
-// WORKS
+// 
 // get book by id
 router.get('/:id', function(req, res, next) {
 	var id = req.params.id;
-  Books().select('*').where('id',id).then(function(book){
-  	res.render('books',{ programming_books: book });
-  })
+	knex.select('first_name','last_name','books_id','authors_id','title','genre','description','cover_url')
+	    .from('authors')
+	    .rightJoin('booklists','authors.id','booklists.authors_id')
+	    .rightJoin('books','books.id','booklists.books_id')
+	    .where('books.id', id)
+	    .then(function(all){
+	    	res.render('books', { authors_books: all });
+	    });
 });
 
 module.exports = router;
